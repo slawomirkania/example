2da772258442d9cce9d92af94c4623c80e3f4f22
Slawomir Kania <slawomir.kania@codete.com>
2018-06-12 12:59:23 +0200

    step normalization/denormalization properties

--- api/src/Entity/Account.php
+++ api/src/Entity/Account.php
@@ -10,9 +10,13 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(attributes={
+ *     "normalization_context"={"groups"={"account.read"}},
+ *     "denormalization_context"={"groups"={"account.write"}}
+ * })
  * @ORM\Entity(repositoryClass="App\Repository\AccountRepository")
  * @ApiFilter(BooleanFilter::class, properties={"isActive"})
  */
@@ -22,25 +26,48 @@ class Account
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({
+     *     "account.read"
+     * })
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
      * @NotBlank(message="Please provide username")
+     * @Groups({
+     *     "account.read",
+     *     "account.write"
+     * })
      */
     private $username;
 
     /**
      * @ORM\Column(type="boolean", options={"default" : false})
+     * @Groups({
+     *     "account.read",
+     *     "account.write"
+     * })
      */
     private $isActive;
 
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Role", mappedBy="accounts")
      * @ApiSubresource(maxDepth=1)
+     * @Groups({
+     *     "account.read"
+     * })
      */
     private $roles;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @NotBlank(message="Please provide password")
+     * @Groups({
+     *     "account.write"
+     * })
+     */
+    private $password;
 
     public function __construct()
     {
@@ -100,6 +127,18 @@ class Account
             $this->roles->removeElement($role);
             $role->removeAccount($this);
         }
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
         return $this;
     }
